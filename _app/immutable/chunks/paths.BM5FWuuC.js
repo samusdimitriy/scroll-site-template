dead05826bd7f726d609b48c2e9dea73a2fa345f
Dimitrij Samus <d.samus@globit.com>
2024-09-14 10:03:22 +0200

    Updates

---
+++ _app/immutable/chunks/paths.BM5FWuuC.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_hymnq0)==null?void 0:s.base)??"/scroll-site-template";var e;const a=((e=globalThis.__sveltekit_hymnq0)==null?void 0:e.assets)??t;export{a,t as b};
---
+++ _app/immutable/entry/start.BEqOWr21.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.ClR-fBCL.js";export{t as start};
